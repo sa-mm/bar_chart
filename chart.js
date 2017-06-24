bf85413718257664ffba4262daf15d5f7065a11b
Sam <mcmyler@gmail.com>
2017-06-24 11:00:06 -0500

    Make responsive

--- chart.js
+++ chart.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', contentLoadedCb)
 
-function contentLoadedCb(req) {
+function contentLoadedCb (req) {
   const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
   req = new XMLHttpRequest()
   req.open('GET', url, true)
@@ -8,14 +8,14 @@ function contentLoadedCb(req) {
   req.onload = function () {
     json = JSON.parse(req.responseText)
     chartIt(json)
-  };
+  }
 }
 
-function chartIt(json) {
+function chartIt (json) {
   const dataset = json.data
 
-  var w = 800;
-  var h = 400;
+  var w = 800
+  var h = 400
   var padding = 60
   var barWidth = 1
 
@@ -43,71 +43,78 @@ function chartIt(json) {
 
   const mungedDataset = years.map((e, i) => [e, gdp[i]]) // [Date, num]
 
-  const tooltip = d3.select("body")
-    .append("div")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
+  const tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('z-index', '10')
+    .style('visibility', 'hidden')
     .attr('id', 'tooltip')
 
   const onMouseOverCB = (d, i) => {
     tooltip.text(d[0])
     tooltip.attr('data-date', dataset[i][0])
-    return tooltip.style("visibility", "visible")
+    return tooltip.style('visibility', 'visible')
   }
 
-  var svg = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h)
-    .attr("id", "title")
+  // var svg = d3.select("body")
+  //   .append("svg")
+  //   .attr("width", w)
+  //   .attr("height", h)
+  //   .attr("id", "title")
 
-  svg.selectAll("rect")
+  // Make it responsive
+  var svg = d3.select('div#container')
+              .append('svg')
+              .attr('preserveAspectRation', 'xMinYMin meet')
+              .attr('viewBox', '0 0 ' + w + ' ' + h)
+              .attr('id', 'title')
+
+  svg.selectAll('rect')
     .data(mungedDataset)
     .enter()
-    .append("rect")
-    .style("fill", "blue")
-    .attr("x", d => xScale(d[0]))
-    .attr("y", (d, i) => yScale(d[1]))
-    .attr("width", (d, i) => barWidth)
-    .attr("height", (d, i) => {
+    .append('rect')
+    .style('fill', 'blue')
+    .attr('x', d => xScale(d[0]))
+    .attr('y', (d, i) => yScale(d[1]))
+    .attr('width', (d, i) => barWidth)
+    .attr('height', (d, i) => {
       return h - padding - yScale(d[1])
     })
     .attr('class', 'bar')
     .attr('data-date', (d, i) => dataset[i][0])
     .attr('data-gdp', d => d[1])
     .text((d, i) => dataset[i][0])
-    .on("mouseover", onMouseOverCB)
-    .on("mousemove", function () { return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px"); })
-    .on("mouseout", function () { return tooltip.style("visibility", "hidden"); });
+    .on('mouseover', onMouseOverCB)
+    .on('mousemove', function () { return tooltip.style('top', (d3.event.pageY - 10) + 'px').style('left', (d3.event.pageX + 10) + 'px') })
+    .on('mouseout', function () { return tooltip.style('visibility', 'hidden') })
 
   const xAxis = d3.axisBottom(xScale)
 
-  svg.append("g")
-    .attr("transform", "translate(0," + (h - padding) + ")")
+  svg.append('g')
+    .attr('transform', 'translate(0,' + (h - padding) + ')')
     .call(xAxis)
-    .attr("id", "x-axis")
+    .attr('id', 'x-axis')
 
   const yAxis = d3.axisLeft(yScale)
 
-  svg.append("g")
-    .attr("transform", "translate(" + padding + ",0)")
+  svg.append('g')
+    .attr('transform', 'translate(' + padding + ',0)')
     .call(yAxis)
-    .attr("id", "y-axis")
+    .attr('id', 'y-axis')
 
-  svg.append("text")
-    .attr("x", (w / 2))
-    .attr("y", padding)
-    .attr("text-anchor", "middle")
-    .style("font-size", "20px")
-    .text("U.S. GDP")
+  svg.append('text')
+    .attr('x', (w / 2))
+    .attr('y', padding)
+    .attr('text-anchor', 'middle')
+    .style('font-size', '20px')
+    .text('U.S. GDP')
 
-  svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0)
-      .attr("x",0 - (h / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Billions of Dollars")
+  svg.append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 0)
+      .attr('x', 0 - (h / 2))
+      .attr('dy', '1em')
+      .style('text-anchor', 'middle')
+      .text('Billions of Dollars')
       .attr('id', 'y-axis-label')
 }
